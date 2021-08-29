b4a244cdb9972d1a39f06796b941f474d972cb26
MRGRD56 <kirgol85@gmail.com>
2021-08-29 14:28:04 +0500

    some improvements

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -5,13 +5,25 @@ import Size from "../../models/Size";
 import Point from "../../models/Point";
 import PropTypes from "prop-types";
 import WindowTitle from "../WindowTitle/WindowTitle";
+import Draggable, {DraggableCore} from "react-draggable";
 
 function Window(props) {
     let [rectangle, setRectangle] = useState(props.rectangle);
 
+    function onDrag(e) {
+        const [x, y] = [e.movementX, e.movementY];
+        setRectangle(new Rectangle(new Point(rectangle.point.x + x, rectangle.point.y + y), rectangle.size));
+    }
+
+    function onDragStop() {
+        if (rectangle.point.y < 0) {
+            setRectangle(new Rectangle(new Point(rectangle.point.x, 0), rectangle.size));
+        }
+    }
+
     return (
         <div className="window d-flex flex-column position-absolute" style={rectangle.style}>
-            <WindowTitle title={props.title}/>
+            <WindowTitle title={props.title} onDrag={onDrag} onDragStop={onDragStop}/>
         </div>
     );
 }
--- src/components/WindowTitle/WindowTitle.js
+++ src/components/WindowTitle/WindowTitle.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./WindowTitle.scss";
 import PropTypes from "prop-types";
 import WindowTitleButton from "../WindowTitleButton/WindowTitleButton";
+import {DraggableCore} from "react-draggable";
 
-function WindowTitle(props) {
+function WindowTitle({title, onDrag, onDragStart, onDragStop, ...props}) {
     return (
-        <div className="d-flex flex-row justify-content-between align-items-center">
-            <div className="ms-2 unselectable" style={{fontSize: "0.95rem"}}>{props.title}</div>
+        <div className="window-title d-flex flex-row justify-content-between align-items-stretch" {...props}>
+            <DraggableCore onDrag={onDrag} onStart={onDragStart} onStop={onDragStop} axis="both">
+                <div className="ps-2 unselectable d-flex align-items-center" style={{fontSize: "0.95rem", flex: 1}}>
+                    {title}
+                </div>
+            </DraggableCore>
             <div className="d-flex flex-nowrap">
                 <WindowTitleButton>
                     <i className="material-icons" style={{position: "relative", top: "-5px"}}>minimize</i>
@@ -23,7 +28,10 @@ function WindowTitle(props) {
 }
 
 WindowTitle.propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    onDrag: PropTypes.func,
+    onDragStart: PropTypes.func,
+    onDragStop: PropTypes.func
 };
 
 export default WindowTitle;
