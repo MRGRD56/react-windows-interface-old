ccec4df571272401be6e8fa0a791d39fbf9ab75b
MRGRD56 <kirgol85@gmail.com>
2021-08-30 08:35:29 +0500

    add window resize

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -26,8 +26,64 @@ function Window(props) {
         }
     }
 
-    function onResize(e) {
-        setRectangle(new Rectangle(rectangle.point, new Size(rectangle.size.width + e.movementX, rectangle.size.height + e.movementY)));
+    function onResize(e, side) {
+        const [isTop, isRight, isBottom, isLeft] = [
+            (side & Side.top) === Side.top,
+            (side & Side.right) === Side.right,
+            (side & Side.bottom) === Side.bottom,
+            (side & Side.left) === Side.left
+        ];
+
+        let [x, y] = [e.movementX, e.movementY];
+        const sizeIncrement = new Size(0, 0);
+        const pointIncrement = new Point(0, 0);
+
+        if (!(isTop && isRight
+            || isRight && isBottom
+            || isBottom && isLeft
+            || isLeft && isTop)) {
+            if (isTop || isBottom) {
+                x = 0;
+            }
+            if (isLeft || isRight) {
+                y = 0;
+            }
+        }
+
+        //Right, Bottom or Bottom-Right
+        if (isBottom && !isLeft || isRight && !isTop) {
+            sizeIncrement.width = x;
+            sizeIncrement.height = y;
+        }
+        //Top, Left or Top-Left
+        else if (isTop && !isRight || isLeft && !isBottom) {
+            pointIncrement.x = x;
+            pointIncrement.y = y;
+            sizeIncrement.width = -x;
+            sizeIncrement.height = -y;
+        }
+        //Top-Right
+        else if (isTop && isRight) {
+            pointIncrement.y = y;
+            sizeIncrement.height = -y;
+            sizeIncrement.width = x;
+        }
+        //Bottom-Left
+        else if (isBottom && isLeft) {
+            pointIncrement.x = x;
+            sizeIncrement.width = -x;
+            sizeIncrement.height = y;
+        }
+
+        setRectangle(
+            new Rectangle(
+                new Point(
+                    rectangle.point.x + pointIncrement.x,
+                    rectangle.point.y + pointIncrement.y
+                ),
+                new Size(
+                    rectangle.size.width + sizeIncrement.width,
+                    rectangle.size.height + sizeIncrement.height)));
     }
 
     let isResizing = false;
@@ -44,21 +100,29 @@ function Window(props) {
         <div className={`window${props.isAcrylic !== false ? " acrylic" : ""} d-flex flex-column position-absolute`}
             style={rectangle.style}>
             <div className="resize-borders-wrapper">
-                <ResizeBorder className="resize-border resize-border-t" onDrag={e => onResize(e, Side.top)}
+                <ResizeBorder className="resize-border resize-border-t"
+                    onDrag={e => onResize(e, Side.top)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-r" onDrag={e => onResize(e, Side.right)}
+                <ResizeBorder className="resize-border resize-border-r"
+                    onDrag={e => onResize(e, Side.right)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-b" onDrag={e => onResize(e, Side.bottom)}
+                <ResizeBorder className="resize-border resize-border-b"
+                    onDrag={e => onResize(e, Side.bottom)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-l" onDrag={e => onResize(e, Side.left)}
+                <ResizeBorder className="resize-border resize-border-l"
+                    onDrag={e => onResize(e, Side.left)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-tl" onDrag={e => onResize(e, Side.top | Side.left)}
+                <ResizeBorder className="resize-border resize-border-tl"
+                    onDrag={e => onResize(e, Side.top | Side.left)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-tr" onDrag={e => onResize(e, Side.top | Side.right)}
+                <ResizeBorder className="resize-border resize-border-tr"
+                    onDrag={e => onResize(e, Side.top | Side.right)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-br" onDrag={e => onResize(e, Side.bottom | Side.left)}
+                <ResizeBorder className="resize-border resize-border-br"
+                    onDrag={e => onResize(e, Side.bottom | Side.right)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
-                <ResizeBorder className="resize-border resize-border-bl" onDrag={e => onResize(e, Side.bottom | Side.right)}
+                <ResizeBorder className="resize-border resize-border-bl"
+                    onDrag={e => onResize(e, Side.bottom | Side.left)}
                     onDragStart={onResizeStart} onDragStop={onResizeStop}/>
             </div>
             <WindowTitle title={props.title} onDrag={onDrag} onDragStop={onDragStop}/>
@@ -72,6 +136,8 @@ Window.propTypes = {
     y: PropTypes.number,
     width: PropTypes.number,
     height: PropTypes.number,
+    minWidth: PropTypes.number,
+    minHeight: PropTypes.number,
     isAcrylic: PropTypes.bool
 };
 
--- src/models/Side.js
+++ src/models/Side.js
@@ -1,8 +1,8 @@
 const Side = {
-    top:    0x0001,
-    right:  0x0010,
-    bottom: 0x0100,
-    left:   0x1000
+    top:    0b0001,
+    right:  0b0010,
+    bottom: 0b0100,
+    left:   0b1000
 };
 
 export default Side;
